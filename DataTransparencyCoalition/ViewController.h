e7de95a98c2ca0d1a93af625321b44750251a774
Weien Wang <weienw@gmail.com>
2015-02-02 11:16:32 -0600

    initial commit

---
+++ DataTransparencyCoalition/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DataTransparencyCoalition
+//
+//  Created by Weien Wang on 2/2/15.
+//  Copyright (c) 2015 Data Transparency Coalition. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ DataTransparencyCoalition/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  DataTransparencyCoalition
+//
+//  Created by Weien Wang on 2/2/15.
+//  Copyright (c) 2015 Data Transparency Coalition. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
