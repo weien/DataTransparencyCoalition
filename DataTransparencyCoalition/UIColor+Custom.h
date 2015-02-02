59dc3014a35b971373e81804ddf976fe1d932b53
Weien Wang <weienw@gmail.com>
2015-02-02 15:09:59 -0600

    [NEW] UIColor+Custom, and some colors

---
+++ DataTransparencyCoalition/UIColor+Custom.h
@@ -0,0 +1,15 @@
+//
+//  UIColor+Custom.h
+//  DataTransparencyCoalition
+//
+//  Created by Weien Wang on 2/2/15.
+//  Copyright (c) 2015 Data Transparency Coalition. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIColor (Custom)
++ (UIColor*) orangeColorSun;
++ (UIColor*) grayColorVeryLight;
++ (UIColor*) grayColorVeryDark;
+@end
---
+++ DataTransparencyCoalition/UIColor+Custom.m
@@ -0,0 +1,24 @@
+//
+//  UIColor+Custom.m
+//  DataTransparencyCoalition
+//
+//  Created by Weien Wang on 2/2/15.
+//  Copyright (c) 2015 Data Transparency Coalition. All rights reserved.
+//
+
+#import "UIColor+Custom.h"
+
+@implementation UIColor (Custom)
++ (UIColor*) orangeColorSun {
+    return [UIColor colorWithRed:0.96 green:0.55 blue:0.22 alpha:1];
+}
+
++ (UIColor*) grayColorVeryLight {
+    return [UIColor colorWithRed:0.95 green:0.95 blue:0.95 alpha:1];
+}
+
++ (UIColor*) grayColorVeryDark {
+    return [UIColor colorWithRed:0.18 green:0.2 blue:0.21 alpha:1];
+}
+
+@end
