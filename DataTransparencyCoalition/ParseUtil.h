e391f2ecc35f406199eb8e0f2004d1bb75fc6631
Weien Wang <weienw@gmail.com>
2015-02-02 17:36:15 -0600

    [NEW] ParseUtil

---
+++ DataTransparencyCoalition/ParseUtil.h
@@ -0,0 +1,13 @@
+//
+//  ParseUtil.h
+//  DataTransparencyCoalition
+//
+//  Created by Weien Wang on 2/2/15.
+//  Copyright (c) 2015 Data Transparency Coalition. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ParseUtil : NSObject
+
+@end
