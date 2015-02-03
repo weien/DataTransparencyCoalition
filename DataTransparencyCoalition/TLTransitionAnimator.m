14900f85b4e9b2e813677e0fc9b5ee0519cfbb29
Weien Wang <weienw@gmail.com>
2015-02-02 22:26:23 -0600

    [ADD] subtle changes to transition (including orange launch background color)

--- DataTransparencyCoalition/TLTransitionAnimator.m
+++ DataTransparencyCoalition/TLTransitionAnimator.m
@@ -11,7 +11,7 @@
 @implementation TLTransitionAnimator
 
 - (NSTimeInterval)transitionDuration:(id <UIViewControllerContextTransitioning>)transitionContext {
-    return 0.2f;
+    return 0.3f;
 }
 
 - (void)animateTransition:(id <UIViewControllerContextTransitioning>)transitionContext {
