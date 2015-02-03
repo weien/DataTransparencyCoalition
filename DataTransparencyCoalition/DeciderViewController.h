7ec75d2ecc71f5d4ddff28974059f07cd536b59e
Weien Wang <weienw@gmail.com>
2015-02-02 20:42:05 -0600

    [NEW] DeciderVC

---
+++ DataTransparencyCoalition/DeciderViewController.h
@@ -0,0 +1,13 @@
+//
+//  DeciderViewController.h
+//  DataTransparencyCoalition
+//
+//  Created by Weien Wang on 2/2/15.
+//  Copyright (c) 2015 Data Transparency Coalition. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DeciderViewController : UIViewController
+
+@end
---
+++ DataTransparencyCoalition/DeciderViewController.m
@@ -0,0 +1,13 @@
+//
+//  DeciderViewController.m
+//  DataTransparencyCoalition
+//
+//  Created by Weien Wang on 2/2/15.
+//  Copyright (c) 2015 Data Transparency Coalition. All rights reserved.
+//
+
+#import "DeciderViewController.h"
+
+@implementation DeciderViewController
+
+@end
