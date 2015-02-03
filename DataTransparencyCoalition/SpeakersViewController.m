3a0ac1b90baeddb6f88395a2b9efbb960d4bda1b
Weien Wang <weienw@gmail.com>
2015-02-03 09:33:59 -0600

    [NEW] SpeakersVC

---
+++ DataTransparencyCoalition/SpeakersViewController.m
@@ -0,0 +1,17 @@
+//
+//  SpeakersViewController.m
+//  DataTransparencyCoalition
+//
+//  Created by Weien Wang on 2/3/15.
+//  Copyright (c) 2015 Data Transparency Coalition. All rights reserved.
+//
+
+#import "SpeakersViewController.h"
+
+@implementation SpeakersViewController
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    //for individual speaker pages, use textview so we can get free URL detection
+}
+@end
