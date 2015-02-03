baffb8977538ec5f401d968fcdc692542611e0e5
Weien Wang <weienw@gmail.com>
2015-02-03 15:14:30 -0600

    [NEW] ProgramSection NSObject subclass

---
+++ DataTransparencyCoalition/ProgramSection.h
@@ -0,0 +1,18 @@
+//
+//  ProgramSection.h
+//  DataTransparencyCoalition
+//
+//  Created by Weien Wang on 2/3/15.
+//  Copyright (c) 2015 Data Transparency Coalition. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ProgramSection : NSObject
+@property (nonatomic, strong) NSString* sectionName;
+@property (nonatomic, strong) NSDate* sectionTime;
+//@property (nonatomic, strong) NSString* sectionTitleText;
+@property (nonatomic, strong) NSArray* sectionItems;
+@property (nonatomic, strong) NSString* sectionSponsor;
+
+@end
