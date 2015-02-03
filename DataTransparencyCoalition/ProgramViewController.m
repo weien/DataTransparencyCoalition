852c56d21b4f62c4cc0f080a81c2854b225259f2
Weien Wang <weienw@gmail.com>
2015-02-03 10:45:21 -0600

    [ADD] basic setup for ProgramVC

--- DataTransparencyCoalition/ProgramViewController.m
+++ DataTransparencyCoalition/ProgramViewController.m
@@ -7,12 +7,51 @@
 //
 
 #import "ProgramViewController.h"
+#import "UIColor+Custom.h"
+#import "DTCUtil.h"
+#import "Constants.h"
 
-@interface ProgramViewController()
+@interface ProgramViewController() <UITableViewDataSource, UITableViewDelegate>
 @property (strong, nonatomic) IBOutlet UITableView *mainTableView;
+
+@property (strong, nonatomic) NSArray *programData;
 
 @end
 
 @implementation ProgramViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    self.mainTableView.delegate = self;
+    self.mainTableView.dataSource = self;
+    self.mainTableView.tableFooterView = [[UIView alloc] initWithFrame:CGRectZero];
+
+}
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+    return 1;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return 0;
+}
+
+-(UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    static NSString *CellIdentifier = @"Cell";
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier];
+    return cell;
+}
+
+- (UIView *)tableView:(UITableView *)tableView viewForHeaderInSection:(NSInteger)section {
+    UIView *sectionHeader = [[UIView alloc] initWithFrame:CGRectMake(0, 0, tableView.frame.size.width, 10)];
+    sectionHeader.backgroundColor = [UIColor greenColor];
+    
+    return sectionHeader;
+}
+
+- (CGFloat)tableView:(UITableView *)tableView heightForHeaderInSection:(NSInteger)section {
+    return 10;
+}
+
 
 @end
