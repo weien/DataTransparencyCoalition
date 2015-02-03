f7472e1d6d1a7c72369bc28fdcaed110c4467f4a
Weien Wang <weienw@gmail.com>
2015-02-02 20:02:27 -0600

    [NEW] Constants.h

---
+++ DataTransparencyCoalition/Constants.h
@@ -0,0 +1,9 @@
+//
+//  Constants.h
+//  DataTransparencyCoalition
+//
+//  Created by Weien Wang on 2/2/15.
+//  Copyright (c) 2015 Data Transparency Coalition. All rights reserved.
+//
+
+#define kConferenceMetadataComponent @"metadata.plist"
