9cc39ba23d39f99e884516e6b44b52a5d6014ca9
Weien Wang <weienw@gmail.com>
2015-02-06 16:54:37 -0600

    [NEW] CustomSpeakerTileCell

---
+++ DataTransparencyCoalition/CustomSpeakerTileCell.h
@@ -0,0 +1,13 @@
+//
+//  CustomSpeakerTileCell.h
+//  DataTransparencyCoalition
+//
+//  Created by Weien Wang on 2/6/15.
+//  Copyright (c) 2015 Data Transparency Coalition. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CustomSpeakerTileCell : UICollectionViewCell
+
+@end
---
+++ DataTransparencyCoalition/CustomSpeakerTileCell.m
@@ -0,0 +1,13 @@
+//
+//  CustomSpeakerTileCell.m
+//  DataTransparencyCoalition
+//
+//  Created by Weien Wang on 2/6/15.
+//  Copyright (c) 2015 Data Transparency Coalition. All rights reserved.
+//
+
+#import "CustomSpeakerTileCell.h"
+
+@implementation CustomSpeakerTileCell
+
+@end
