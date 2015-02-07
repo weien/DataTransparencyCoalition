b17a977d61b30fcd06c6137f78e896b856ea74b3
Weien Wang <weienw@gmail.com>
2015-02-06 20:49:03 -0600

    [NEW] IndividualVC

---
+++ DataTransparencyCoalition/IndividualViewController.m
@@ -0,0 +1,13 @@
+//
+//  IndividualViewController.m
+//  DataTransparencyCoalition
+//
+//  Created by Weien Wang on 2/6/15.
+//  Copyright (c) 2015 Data Transparency Coalition. All rights reserved.
+//
+
+#import "IndividualViewController.h"
+
+@implementation IndividualViewController
+
+@end
