18dcff68374167001cc546f8d7335f52ae1e3a15
Weien Wang <weienw@gmail.com>
2016-07-14 08:37:24 -0500

    [ADD] fleshing out of retrieveMetadata... and a bit of retrieveHome... from Backendless

--- DataTransparencyCoalition/BackendlessWebService.m
+++ DataTransparencyCoalition/BackendlessWebService.m
@@ -10,6 +10,7 @@
 #import "Backendless.h"
 #import "Metadata.h"
 #import "Home.h"
+#import "Conference.h"
 
 @implementation BackendlessWebService
 + (BackendlessWebService*) sharedInstance {
@@ -23,55 +24,72 @@
 
 - (NSDictionary*) retrieveAppMetadata {
     Fault* fault = nil;
-    BackendlessCollection *returnedCollection = [[Backendless sharedInstance].persistenceService find:[Home class] dataQuery:nil error:&fault];
-    
-//    [[[Backendless sharedInstance].persistenceService of:[Home class]] findFirst:^(id first) {
-//        NSLog(@"first is %@", first);
-//    } error:^(Fault *fault) {
-//        NSLog(@"fault is %@", fault);
-//    }];
-    
-//    @try {
-//        id<IDataStore> dataStore = [[[Backendless sharedInstance] persistenceService] of:[Home class]];
-//        BackendlessCollection *returnedCollection = [dataStore findFirst];
-//        if ([returnedCollection isKindOfClass:[NSObject class]]) {
-//            NSLog(@"BEL returned %@", returnedCollection);
-//        }
-//    }
-//    @catch (Fault *fault){
-//        NSLog(@"Fault is %@", fault);
-//    }
-    
-    return nil;
-    
-    
-    /*
-    PFQuery *query = [PFQuery queryWithClassName:@"Metadata"];
-    [query includeKey:@"currentConference"];
-    
-    NSError* error = nil;
-    PFObject* metadata = [query getFirstObject:&error];
-    if (error) {
-        NSLog(@"Error fetching metadata: %@", error);
-    }
+    BackendlessCollection *collection = [[Backendless sharedInstance].persistenceService find:[Metadata class] dataQuery:nil error:&fault];
     
     NSMutableDictionary* returnDict = [NSMutableDictionary dictionary];
-    if (metadata[@"conferenceModeEnabled"]) {
-        PFObject* conference = metadata[@"currentConference"];
-        //NSLog(@"conference is %@", conference);
+    Metadata* md = collection.data.firstObject;
+    if (md.conferenceModeEnabled) {
+        Conference* conference = md.currentConference;
         
-        for (NSString * key in [conference allKeys]) {
-            [returnDict setObject:[conference objectForKey:key] forKey:key];
-        }
-        [returnDict setObject:@(YES) forKey:@"conferenceModeEnabled"];
-        [returnDict setObject:conference.objectId forKey:@"conferenceId"];
+        //use setValue instead of setObject in case properties are nil
+        [returnDict setValue:@(YES) forKey:@"conferenceModeEnabled"];
+        [returnDict setValue:conference.coalitionURL forKey:@"coalitionURL"];
+        [returnDict setValue:conference.objectId forKey:@"conferenceId"];
+        [returnDict setValue:conference.conferenceURL forKey:@"conferenceURL"];
+        [returnDict setValue:conference.conferenceDate forKey:@"date"];
+        [returnDict setValue:conference.hashtag forKey:@"hashtag"];
+        [returnDict setValue:conference.location forKey:@"location"];
+        [returnDict setValue:conference.mapURL forKey:@"mapURL"];
+        [returnDict setValue:conference.name forKey:@"name"];
+        [returnDict setValue:conference.tweetURL forKey:@"tweetURL"];
     }
     else {
-        //For future development
         [returnDict setObject:@(NO) forKey:@"conferenceModeEnabled"];
     }
-    
     return returnDict;
-     */
 }
+
+- (NSArray*) retrieveHomeDataForConference:(NSString*)conferenceId {
+    Fault* fault = nil;
+    BackendlessDataQuery *query = [BackendlessDataQuery query];
+    query.whereClause = [NSString stringWithFormat:@"conference = %@", conferenceId];
+    query.queryOptions = [QueryOptions query:999 offset:0];
+    BackendlessCollection *metadata = [[Backendless sharedInstance].persistenceService find:[Home class]
+                                                                                  dataQuery:query
+                                                                                      error:&fault];
+
+//    PFQuery *query = [PFQuery queryWithClassName:@"Home"];
+//    PFObject *pointerToConference = [PFObject objectWithoutDataWithClassName:@"Conference" objectId:conferenceId];
+//    [query whereKey:@"conference" equalTo:pointerToConference];
+//    
+//    NSError* error = nil;
+//    NSArray* dataFromParse = [query findObjects:&error];
+//    if (error) {
+//        NSLog(@"Error fetching home data: %@", error);
+//    }
+//    
+    NSMutableArray* returnArr = [NSMutableArray array];
+//    for (PFObject* obj in dataFromParse) {
+//        NSMutableDictionary* dictToAdd = [NSMutableDictionary dictionary];
+//        for (NSString * key in [obj allKeys]) {
+//            id subObj = [obj objectForKey:key];
+//            if ([subObj isKindOfClass:[PFObject class]]) {
+//                PFObject* pfObj = (PFObject*)subObj;
+//                [dictToAdd setObject:pfObj.objectId forKey:key];
+//            }
+//            else if ([subObj isKindOfClass:[PFFile class]]) {
+//                PFFile* pfFile = (PFFile*)subObj;
+//                [dictToAdd setObject:pfFile.url forKey:key];
+//            }
+//            else {
+//                [dictToAdd setObject:[obj objectForKey:key] forKey:key];
+//            }
+//        }
+//        [returnArr addObject:dictToAdd];
+//    }
+    
+    return returnArr;
+}
+
+
 @end
